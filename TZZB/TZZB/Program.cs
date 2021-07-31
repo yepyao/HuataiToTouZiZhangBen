471c813e120b96bbf3c44e326277e5025e89ee56
Yepyao <yepyao@gmail.com>
2021-07-31 19:13:33 +0800

    fix some bugs

--- TZZB/TZZB/Program.cs
+++ TZZB/TZZB/Program.cs
@@ -10,31 +10,43 @@ namespace TZZB
     class Program
     {
         static string folderPath = @"C:\Users\yepyao\Documents\stocklist";
+
+        static string outputHeader = "成交日期, 证券代码, 证券名称, 交易类别, 成交数量, 成交价格, 发生金额, 证券余额, 佣金, 税费";
+
         static void Main(string[] args)
         {
+            Directory.Delete(Path.Combine(folderPath, "output"), true);
+            Directory.CreateDirectory(Path.Combine(folderPath, "output"));
+
+            StreamWriter streamWriter = new StreamWriter(Path.Combine(folderPath, "output", "all.csv"), false, System.Text.Encoding.GetEncoding(936));
+            streamWriter.WriteLine(outputHeader);
+
             foreach (string filename in Directory.EnumerateFiles(folderPath))
             {
-                TransferFormat(filename);
+                TransferFormat(filename, streamWriter);
             }
+            streamWriter.Close();
         }
 
         static string[] TypeFilter = new string[]
         {
             "证券卖出",
-            "证券买入"
+            "证券买入",
+            "新股入帐",
+            "转托转入"
         };
 
-        static void TransferFormat(string filename)
+        static void TransferFormat(string filename, StreamWriter streamWriter)
         {
             string folder = Path.GetDirectoryName(filename);
 
             StreamReader reader = new StreamReader(filename, System.Text.Encoding.GetEncoding(936));
 
-            StreamWriter writer = new StreamWriter(Path.Combine(folder, Path.GetFileNameWithoutExtension(filename)) + ".csv", false, System.Text.Encoding.GetEncoding(936));
+            StreamWriter writer = new StreamWriter(Path.Combine(folder, "output", Path.GetFileNameWithoutExtension(filename)) + ".csv", false, System.Text.Encoding.GetEncoding(936));
 
             // read header first
             string line = reader.ReadLine();
-            writer.WriteLine("成交日期, 证券代码, 证券名称, 交易类别, 成交数量, 成交价格, 发生金额, 证券余额");
+            writer.WriteLine(outputHeader);
 
             while ((line = reader.ReadLine()) != null)
             {
@@ -43,9 +55,48 @@ namespace TZZB
 
                 if (!TypeFilter.Contains(columns[4]))
                 {
-                    //continue;
+                    continue;
                 }
-                writer.WriteLine(string.Join(", ",
+
+
+                if (columns[2].StartsWith("11") || columns[2].StartsWith("132"))
+                {
+                    columns[5] += "0";
+                }
+
+                string calcAmount = (Math.Abs(int.Parse(columns[5])) * double.Parse(columns[6])).ToString("F3");
+
+                if (columns[4] == "新股入帐")
+                {
+                    columns[4] = "证券买入";
+                    columns[7] = calcAmount;
+                }
+
+                if (columns[4] == "转托转入")
+                {
+                    columns[4] = "证券买入";
+                    columns[7] = calcAmount;
+                }
+
+                bool attentionNeed = false;
+                if (columns[7] != calcAmount)
+                {
+                    double calcAmountParsed = double.Parse(calcAmount);
+                    double realAmountParsed = double.Parse(columns[7]);
+
+                    if (Math.Abs(calcAmountParsed - realAmountParsed) / Math.Min(calcAmountParsed, realAmountParsed) > 0.03)
+                    {
+                        attentionNeed = true;
+                    }
+                }
+
+                if (attentionNeed)
+                {
+                    Console.WriteLine("!!! " + line);
+                    Console.WriteLine("\tcalc:\t" + calcAmount + "\treal:\t" + columns[7]);
+                }
+
+                string outline = string.Join(", ",
                     columns[0], //成交日期
                     columns[2], //证券代码
                     columns[3], //证券名称
@@ -53,8 +104,12 @@ namespace TZZB
                     columns[5], //成交数量
                     columns[6], //成交价格
                     columns[7], //发生金额
-                    columns[18] //证券余额
-                ));
+                    columns[18], //证券余额
+                    columns[8], //佣金
+                    (double.Parse(columns[9])+double.Parse(columns[10])).ToString("F3") //税费
+                );
+                writer.WriteLine(outline);
+                streamWriter.WriteLine(outline);
             }
             reader.Close();
             writer.Close();
